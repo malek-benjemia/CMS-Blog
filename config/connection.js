7bd273435ae0e0a5f7d8550dd6ad8a28744bfa97
MalekBenJemia <malekbenjemia@gmail.com>
2021-03-14 19:21:51 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    hHOST: "us-cdbr-east-03.cleardb.com",
+    HOST: "us-cdbr-east-03.cleardb.com",
     USER: "bb3d780745e9d6",
     PASSWORD: "8db37276",
     DB: "heroku_dc474aa494420c1",
