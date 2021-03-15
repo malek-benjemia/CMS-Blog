1368ee417ab747345d32ea8d73bea562d9aa0d71
MalekBenJemia <malekbenjemia@gmail.com>
2021-03-14 20:40:57 -0400

    added some css for the cards

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,18 +3,18 @@ const { Comment } = require('../models');
 const commentdata = [
   {
     comment_text: 'I changed my mind and dont like apples.',
-    user_id: 1,
-    post_id: 1
+    user_id: 4,
+    post_id: 4
   },
   {
     comment_text: 'I like oranges now.',
-    user_id: 1,
-    post_id: 1
+    user_id: 4,
+    post_id: 4
   },
   {
     comment_text: 'This thread is not intresting.',
-    user_id: 2,
-    post_id: 1
+    user_id: 4,
+    post_id: 4
   }
 ];
 
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,17 +4,22 @@ const postdata = [
   {
     title: 'I love apples',
     content: 'Apples are the best fruits in the universe.',
-    user_id: 1
+    user_id: 4
   },
   {
     title: 'I bought a cat.',
     content: 'I wanted a cat since a long time. I finally bought one.',
-    user_id: 1
+    user_id: 4
   },
   {
     title: 'Who can lend me his car?',
     content: 'I need to go to Montreal.',
-    user_id: 2
+    user_id: 4
+  },
+  {
+    title: 'Who can lend me his jet?',
+    content: 'I need to go to Paris.',
+    user_id: 4
   }
 ];
 
